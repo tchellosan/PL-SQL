c4b1c6eef8bba5750e16143c1df77b22cccc2b16
Marcelo Silva <tchellosan@gmail.com>
2020-02-17 17:25:44 -0300

    create table update

--- create table.sql
+++ create table.sql
@@ -17,3 +17,16 @@ alter table emp
 
 alter table emp
 	modify salario 	number(10,2) not null
+
+-------------------------------------------------------------------
+
+create table regions (
+	region_id number,
+	region_name varchar2(60),
+	primary key (region_id)
+);
+
+insert into regions values(1, 'europe');
+insert into regions values(2, 'americas');
+insert into regions values(3, 'asia');
+insert into regions values(4, 'middle east and africa');
