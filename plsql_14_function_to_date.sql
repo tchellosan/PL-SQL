486946014f25f7fbc8ae9bda1a4a02ed75028dc3
Marcelo Silva <tchellosan@gmail.com>
2020-02-18 16:36:18 -0300

    funcion to_date()

---
+++ plsql_14_function_to_date.sql
@@ -0,0 +1,28 @@
+clear scr;
+set serveroutput on;
+
+declare
+	wdata date;
+	wvalor number;
+begin
+
+	dbms_output.put_line(to_date('010682', 'ddmmrr'));
+	dbms_output.put_line(to_date('21.05.2009', 'dd.mm.yyyy'));
+	dbms_output.put_line(to_date('April 21', 'month dd', 'nls_date_language=american'));
+	dbms_output.put_line(to_date('Abril 21', 'month dd', 'nls_date_language=''BRAZILIAN PORTUGUESE'''));
+--	dbms_output.put_line(to_date('Abril 21', 'month XX', 'nls_date_language=''BRAZILIAN PORTUGUESE'''));
+	
+	dbms_output.put_line('-');
+	dbms_output.put_line(to_char(sysdate, 'DD/MM/YYYY HH24:MI:SS'));
+	dbms_output.put_line(to_date('2008', 'yyyy'));
+	dbms_output.put_line(to_date(60, 'ddd'));
+
+	dbms_output.put_line('-');
+	dbms_output.put_line(to_char(sysdate, 'dd/mm/yyyy'));
+	dbms_output.put_line(to_char(to_date('01/01/49', 'dd/mm/yy'), 'dd/mm/yyyy'));	
+	dbms_output.put_line(to_char(to_date('01/01/50', 'dd/mm/yy'), 'dd/mm/yyyy'));	
+	dbms_output.put_line(to_char(to_date('01/01/49', 'dd/mm/rr'), 'dd/mm/rrrr'));	
+	dbms_output.put_line(to_char(to_date('01/01/50', 'dd/mm/rr'), 'dd/mm/rrrr'));		
+
+end;
+/
